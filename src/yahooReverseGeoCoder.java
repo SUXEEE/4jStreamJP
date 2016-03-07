1b37b988be531d253c6312dcdcc95f0104900878
SUXEEE <sue@cps.im.dendai.ac.jp>
2016-03-07 10:58:25 +0900

    Added & Fixed yGeoCorder API key.
    Use java.util.ResourceBundle.

--- src/yahooReverseGeoCoder.java
+++ src/yahooReverseGeoCoder.java
@@ -1,17 +1,11 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ResourceBundle;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.apache.commons.jxpath.JXPathContext;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 
 public class yahooReverseGeoCoder {
@@ -22,8 +16,8 @@ public class yahooReverseGeoCoder {
 		File file = new File("yGeo.xml");
 		
 		try {
-						
-			URL url = new URL("http://reverse.search.olp.yahooapis.jp/OpenLocalPlatform/V1/reverseGeoCoder?lat="+lat+"&lon="+lng+"&appid=dj0zaiZpPTRITWljcDkzdExTaCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGE-");
+			String yGeoCorder = ResourceBundle.getBundle("yGeoCorder").getString("apiKey");
+			URL url = new URL("http://reverse.search.olp.yahooapis.jp/OpenLocalPlatform/V1/reverseGeoCoder?lat="+lat+"&lon="+lng+yGeoCorder);
 			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 			String strTemp = "";
 			//String yXml = "";
